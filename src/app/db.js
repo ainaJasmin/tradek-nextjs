5bbe4efdd5436c5fe6816346d96a3d0b86c01929
Parsa Rafiee <ec221085@qmul.ac.uk>
2024-04-02 17:00:45 +0100

    db fixed

--- src/app/db.js
+++ src/app/db.js
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAymbWHb07x6Bg7y8gbO78b4IbvVR96pqg",
+  authDomain: "swe35-c8e59.firebaseapp.com",
+  projectId: "swe35-c8e59",
+  storageBucket: "swe35-c8e59.appspot.com",
+  messagingSenderId: "273459700532",
+  appId: "1:273459700532:web:b4ce94b6dc0df49e2f3b61"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
